6dd4dbaa52d4a074f93751cc955767a805ce5c13
Yauhen <zhendos@tut.by>
2020-10-07 23:59:52 +0200

    added build folder

---
+++ build/precache-manifest.63d491e253ddf3641899047ba80126c9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07bcb2c29a6a4bee667694df9b2c115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d37f4da85ebabeb06c3e",
+    "url": "/static/css/main.7e2b8eb6.chunk.css"
+  },
+  {
+    "revision": "2d55059966b0c82d2a73",
+    "url": "/static/js/2.79c73ab7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79c73ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37f4da85ebabeb06c3e",
+    "url": "/static/js/main.de2057e0.chunk.js"
+  },
+  {
+    "revision": "0ca21fa1f4d2797381aa",
+    "url": "/static/js/runtime-main.9d750829.js"
+  },
+  {
+    "revision": "73ea712e9c069ddfcc3747b0cf15196f",
+    "url": "/static/media/Rubik-Medium.73ea712e.woff"
+  },
+  {
+    "revision": "d29ffa65b0cee9258597fe5ddeea6505",
+    "url": "/static/media/Rubik-Medium.d29ffa65.woff2"
+  },
+  {
+    "revision": "344e2175874921c983e60ec209a674e3",
+    "url": "/static/media/Rubik-Regular.344e2175.woff2"
+  },
+  {
+    "revision": "802f6a1271af550cb54cbfc2c462f165",
+    "url": "/static/media/Rubik-Regular.802f6a12.woff"
+  },
+  {
+    "revision": "7c66c472eb50c9abe027977b6f2b406e",
+    "url": "/static/media/bg-dots.7c66c472.svg"
+  },
+  {
+    "revision": "ba45f6788c38e8a4fc487eab6847a365",
+    "url": "/static/media/icon-error.ba45f678.svg"
+  },
+  {
+    "revision": "25a237330ac7fab409f2b5833363ae97",
+    "url": "/static/media/illustration-features-tab-1.25a23733.svg"
+  },
+  {
+    "revision": "dfa0cd821469019e297c8c27cfc0d475",
+    "url": "/static/media/illustration-hero.dfa0cd82.svg"
+  },
+  {
+    "revision": "09a53a4b326d63ee7af9bab0e0fd5605",
+    "url": "/static/media/logo-bookmark-light.09a53a4b.svg"
+  },
+  {
+    "revision": "53989d2a42baf707e1d92f5d05c7899b",
+    "url": "/static/media/logo-bookmark.53989d2a.svg"
+  },
+  {
+    "revision": "3e1ab00bc1a7182ca9297e73eaa98480",
+    "url": "/static/media/logo-chrome.3e1ab00b.svg"
+  },
+  {
+    "revision": "e5023c2b57490fa1d28da6dea398606b",
+    "url": "/static/media/logo-firefox.e5023c2b.svg"
+  },
+  {
+    "revision": "3d8f7bcc99ea84508c6a3380e0ae0878",
+    "url": "/static/media/logo-opera.3d8f7bcc.svg"
+  }
+]);
